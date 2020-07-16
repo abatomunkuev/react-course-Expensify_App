efbc2b258f73f40952bbe9dab97ba09ba6a8cbeb
abatomunkuev <abatomunkuev@myseneca.ca>
2020-07-16 16:55:30 -0400

    Fixed database URL issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,19 @@
-import * as firebase from 'firebase'; // * as - it takes all of the named exports from firebase and put them into the variable firebase
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/auth";
+import 'firebase/database'; // * as - it takes all of the named exports from firebase and put them into the variable firebase
 import { set } from 'numeral';
 
 const firebaseConfig = {
-    apiKey:process.env.FIREBASE_API_KEY,
-    authDomain:process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL:process.env.FIREBASE_DATABASE_URL,
-    projectId:process.env.FIREBASE_PROJECT_ID,
-    storageBucket:process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
+
+console.log(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
-  } else if (process.env.NODE_ENV === 'development') {
+} else if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' });
-  }
+}
 
 module.exports = (env) => {
     const isProduction = env === 'production';
@@ -38,12 +38,12 @@ module.exports = (env) => {
         plugins: [
             new MiniExtractPlugin(),
             new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_KEY),
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
                 'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
             })
         ],
         devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
