83c0247dc70fd1fde75579d45b1f8dddbacb216e
abatomunkuev <abatomunkuev@myseneca.ca>
2020-07-17 00:01:46 -0400

    Added startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
     removeExpense,
     setExpenses,
     startSetExpenses, 
-    startRemoveExpense
+    startRemoveExpense,
+    startEditExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -56,6 +57,26 @@ test('should setup editExpense action object', () => {
         updates: {
             note: 'new note'
         }
+    })
+})
+
+test('should edit expenses from firebase', (done) => {
+    const store = createMockStore();
+    const id = expenses[0].id;
+    const updates = {
+        description: 'Rent car'
+    }
+    store.dispatch(startEditExpense(id,updates)).then(() => {
+        const action = store.getActions();
+        expect(action[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val().description).toBe(updates.description);
+        done();
     })
 })
 
